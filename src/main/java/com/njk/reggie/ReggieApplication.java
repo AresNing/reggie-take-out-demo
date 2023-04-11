02a158042c423c36b1e8244135661fd799a63cd0
AresNing <1418226860@qq.com>
2023-04-11 23:47:24 +0800

    Add: 登录检查过滤器

--- src/main/java/com/njk/reggie/ReggieApplication.java
+++ src/main/java/com/njk/reggie/ReggieApplication.java
@@ -3,6 +3,7 @@ package com.njk.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Created with Intellij IDEA
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
