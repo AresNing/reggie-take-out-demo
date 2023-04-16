5df753099ca45b0bfdaadb3624dc21eb5195c98d
AresNing <1418226860@qq.com>
2023-04-16 10:27:11 +0800

    Add: 分类信息查询

--- src/main/java/com/njk/reggie/controller/CategoryController.java
+++ src/main/java/com/njk/reggie/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.njk.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.njk.reggie.common.R;
 import com.njk.reggie.entity.Category;
 import com.njk.reggie.service.CategoryService;
@@ -24,10 +26,40 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 新增分类
+     * @param category 分类信息
+     * @return 新增结果
+     */
     @PostMapping
     public R<String> save(@RequestBody Category category) {
         log.info("新增分类: {}", category);
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+    /**
+     * 分页查询
+     * @param page 页码
+     * @param pageSize 每页显示的条数
+     * @return 分页数据
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        log.info("page = {}, pageSize = {}", page, pageSize);
+
+        // 构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        // 构造条件构造器
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        // 添加排序条件，根据sort排序
+        wrapper.orderByAsc(Category::getSort);
+
+        // 执行查询
+        categoryService.page(pageInfo, wrapper);
+
+        return R.success(pageInfo);
+
     }
 }
