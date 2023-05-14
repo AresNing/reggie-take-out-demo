d6c67113c4cdbb6099e199a60fd562962b83ae92
AresNing <1418226860@qq.com>
2023-05-14 16:33:44 +0800

    Add: 套餐分页查询

--- src/main/java/com/njk/reggie/controller/SetmealController.java
+++ src/main/java/com/njk/reggie/controller/SetmealController.java
@@ -1,14 +1,20 @@
 package com.njk.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.njk.reggie.common.R;
 import com.njk.reggie.dto.SetmealDto;
+import com.njk.reggie.entity.Category;
+import com.njk.reggie.entity.Setmeal;
+import com.njk.reggie.service.CategoryService;
 import com.njk.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created with Intellij IDEA
@@ -27,6 +33,9 @@ public class SetmealController {
     @Autowired
     private SetmealService setmealService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @PostMapping
     public R<String> save(@RequestBody SetmealDto setmealDto) {
         log.info("套餐信息：{}", setmealDto);
@@ -34,5 +43,51 @@ public class SetmealController {
         return R.success("新增套餐成功");
     }
 
+    /**
+     * 套餐分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        // 构造分页构造器
+        Page<Setmeal> pageInfo = new Page<>(page, pageSize);
+        Page<SetmealDto> dtoPage = new Page<>();
+
+        // 构造条件构造器
+        LambdaQueryWrapper<Setmeal> wrapper = new LambdaQueryWrapper<>();
+        // 根据过滤条件，按照name模糊查询
+        wrapper.like(name != null, Setmeal::getName, name);
+        // 添加排序条件，根据update_time排序
+        wrapper.orderByDesc(Setmeal::getUpdateTime);
+        // 执行分页查询
+        setmealService.page(pageInfo, wrapper);
+
+        // 对象拷贝
+        BeanUtils.copyProperties(pageInfo, dtoPage, "records");
+
+        List<Setmeal> records = pageInfo.getRecords();
+        List<SetmealDto> setmealDtoList = records.stream().map(setmeal -> {
+            SetmealDto setmealDto = new SetmealDto();
+            // 对象拷贝
+            BeanUtils.copyProperties(setmeal, setmealDto);
+            // 分类id
+            Long categoryId = setmeal.getCategoryId();
+            // 根据分类id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                //分类名称
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(setmealDtoList);
+
+        return R.success(dtoPage);
+    }
 
 }
